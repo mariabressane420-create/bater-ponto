813202fd009dd7f61d03cb19fcbf305de85d79a4
mariabressane420-create <mariabressane420@gmail.com>
2026-09-09 22:04:33 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = 'https://mariabressane420-create.supabase.co';
+const SUPABASE_URL = 'https://vndjbcjgcqeeyowyuagl.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_Jo7_t82NNEBJB8RaCChezg_Z1D8oa29';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
